250b6db1ac928c64424e928a7965075e6ca4afbd
utkarsh <utkarshpandey519@gmail.com>
2023-11-17 12:59:24 +0530

    This commmit contains the beginning of exception handling in python

--- Ch-22(OOPS)/20_ThreadDemo.py
+++ Ch-22(OOPS)/20_ThreadDemo.py
@@ -1,18 +1,34 @@
-class Hello:
+from time import sleep 
+from threading import *
+
+# Hello thread
+# Hi thread
+# Main thread
+
+class Hello(Thread):
     def run(self):
-        for i in range (5):
-         print("Hello")
+        for i in range(500):
+          print("Hello")
+          sleep(2)
 
 
-
-class Hi:
+class Hi(Thread):
     def run(self):
-        for i in range (5):
+        for i in range(500):
            print("Hi")
+           sleep(2)
 
 
-obj1=Hello()
-obj2=Hi()
+obj1=Hello() # Hello thread created
+obj2=Hi()   # Hi thread created 
 
-obj1.run()
-obj2.run()
+print("Wellcome to the session")
+print("We are learning threads")
+x=9
+
+obj1.start() # Hello thread to start
+sleep(1)
+obj2.start()
+
+sleep(6)
+print("Bye")
